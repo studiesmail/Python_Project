a1c50077d4092f36a0a274bbeb37b46b630442dc
Studies Mail <studiesmail12@gmail.com>
2026-02-07 21:31:25 +0530

    Add files via upload

---
+++ mathematical_operators.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+
+print("Addition: ", num1+num2)
+print("Subtraction: ", num1-num2)
+print("Multiplication: ", num1*num2)
+
+if num2 != 0:
+    print("Division: ", num1 / num2)
+else:
+    print("Division: Cannot Divisible by 0")
---
+++ personalized_greeting.py
@@ -0,0 +1,8 @@
+first_name = input("Enter First name: ")
+second_name = input("Enter Second name: ")
+
+full_name = first_name + " " + second_name
+
+print("Hello,", full_name+"! Welcome to the Python")
+
+
